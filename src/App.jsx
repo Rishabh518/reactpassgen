bf65948166ae3a42e7f0e07701458140467fc328
Rishabh Tanwar <rishabh@Rishabhs-MacBook-Air.local>
2025-02-12 17:31:47 +0530

    final commmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,58 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import './App.css'
 
 
 
 function App() {
 
+  let numbers="123456789";
+  let symbol="$!@%^&*(){}_+=-";
+  let char="abcdefghijklmnopqrstuvwxyzABCCDEFHIJKLMNOPQRSTUVWXYZ";
 
   const [password,setpswd]=useState("")
   const[numallow,setnumallow]=useState(false)
   const[symallow,setsymallow]=useState(false)
-  let numbers="123456789";
-  let symbol="$!@%^&*(){}_+=-";
-  let char="abcdefghijklmnopqrstuvwxyzABCCDEFHIJKLMNOPQRSTUVWXYZ";
-  let sn="",ss="",sc=""
+  const [length,setlength] = useState(8)
 
-  const genpswd=()=>{
-    for(let i=0;i<5;i++){
-    sn += numbers[Math.floor(Math.random()*numbers.length)];
-    ss += symbol[Math.floor(Math.random()*symbol.length)];
-    sc += char[Math.floor(Math.random()*char.length)];
+  const passgenerator = useCallback(()=>{
+    let pass =""
+    let str=char
+    if(numallow){
+      str+=numbers
     }
-    let pss=sc+sn+ss
-    let pswd= pss.split("").sort(()=> Math.random()-0.5).join("")
-    setpswd(pswd)
-  }
-
-  const onlychar= ()=>{
-    for(let i=0;i<5;i++){
-      sc += char[Math.floor(Math.random()*char.length)];
+    if(symallow){
+      str+=symbol
     }
-    let pss=sc
-    let pswd= pss.split("").sort(()=> Math.random()-0.5).join("")
-    setpswd(pswd)
-  }
-
-  const charsym= ()=>{
-    for(let i=0;i<5;i++){
-      sc += char[Math.floor(Math.random()*char.length)];
-      ss += symbol[Math.floor(Math.random()*symbol.length)];
+    for(let i =0;i<length;i++){
+      let n = Math.floor(Math.random()*str.length)
+      pass+=str[n]
     }
-    let pss=sc+ss
-    let pswd= pss.split("").sort(()=> Math.random()-0.5).join("")
-    setpswd(pswd)
+    setpswd(pass)
   }
+  ,[numallow,symallow,length])
 
-  const charnum = ()=>{
-    for(let i=0;i<5;i++){
-      sc += char[Math.floor(Math.random()*char.length)];
-      sn += numbers[Math.floor(Math.random()*numbers.length)];
-    }
-    let pss = sc+sn
-    let pswd= pss.split("").sort(()=> Math.random()-0.5).join("")
-    setpswd(pswd)
-  }
-
-const geneartor= ()=>{
-  if(numallow==false && symallow==false){
-    onlychar()
-  }
-  if(numallow==true){
-    charnum();
-  }
-  if(symallow==true){
-    charsym()
-  }
-  if(numallow==true && symallow==true){
-    genpswd()
-  }
-  
-}
 
 useEffect(()=>{
-geneartor()
-},[numallow,symallow])
+passgenerator()
+},[numallow,symallow,setpswd,length])
 
+useEffect(() => {
+  setpswd(""); 
+}, []);
 
   return (
     <>
     <h1 className='text-xl text-white' >password geneartor</h1>
-    <h3>Generated password --- {password} </h3>
+    <h3>Generated Password: {password || "No password generated yet"}</h3>
+
     
-    numbers<input className='m-2' type="checkbox" onChange={()=>setnumallow(!numallow)} />
-    special symbol <input type="checkbox" onChange={()=>setsymallow(!symallow)}  />
-    <button className='m-5'  onClick={geneartor}> generaor</button>
+    numbers<input className='m-2' type="checkbox"  onChange={()=>setnumallow(!numallow)} />
+    special symbol <input type="checkbox" className='m-2' onChange={()=>setsymallow(!symallow)}  />
+    {length}<input type="range"  className='w-20 m-2 bg-gray-300 text-black' onChange={(e) => setlength(Number(e.target.value))} value={length} min="4"
+          max="20"/>
+    <button className='m-5'  onClick={passgenerator}> generaor</button>
+   
     </>
   )
 }
-
 export default App
